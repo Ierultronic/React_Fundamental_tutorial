3305e52212c3bdd520a2e48591bae3668252918c
Amierul Izzuddin <amierulizzuddin21@gmail.com>
2022-02-19 23:05:59 +0800

    State Manipulating

--- src/components/comClas.js
+++ src/components/comClas.js
@@ -9,21 +9,36 @@ class Learning extends Component{
                 {name:'Manggakali dia ni', weight:'420gm'},
                 {name:'alololololop', weight:'320gm'},
                 {name:'malu sama teman sayunk', weight:'690gm'},
+            ],
+            stats:[
+                {mesej:'Sebelum tukar state guna button tiddies'}
             ]
         }
     }
 clickHandler = () => {
-    alert('hehe tiddies')
+  //  alert('hehe tiddies')
+  this.setState({
+    buah:[
+        {name:'Stay', weight:'420gm'},
+        {name:'Hydrated', weight:'320gm'},
+        {name:'MuhFucker', weight:'690gm'},
+    ],
+    stats:[
+        {mesej:'Selepas tukar state guna button tiddies'}
+    ]
+  })
 }
 
     render(){
         return(
             <div>
+
                 <p>check dis out: <button onClick={this.clickHandler}>( . Y . )</button></p>
                 <h3>-ni adalah class component- contoh pakai state macamni: </h3>
-                <h3>{this.state.buah[0].name}</h3>
-                <h3>{this.state.buah[1].name}</h3>
-                <h3>{this.state.buah[2].name}</h3>
+                <p>{this.state.stats[0].mesej}:</p>
+                <h3>-{this.state.buah[0].name}</h3>
+                <h3>-{this.state.buah[1].name}</h3>
+                <h3>-{this.state.buah[2].name}</h3>
             </div>
         ) 
     }
