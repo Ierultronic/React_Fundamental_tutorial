a9d1f304508bf8070605f9c8ddfe79a417e1e084
Amierul Izzuddin <amierulizzuddin21@gmail.com>
2022-02-20 20:48:01 +0800

    UseState Hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ComFunct from './components/Hello';
 import Comclass from './components/comClas';
+import HookState from './components/Hookstate';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
+        <HookState/>
         <ComFunct name='{ini adalah props}'/>
         <Comclass/>
       </header>
