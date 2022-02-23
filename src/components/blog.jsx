722c6c0cbd15a277e148a9575f1d6ee3326681f2
Amierul Izzuddin <amierulizzuddin21@gmail.com>
2022-02-23 20:55:24 +0800

    Fetch Data from API using Axios -すばらしい-

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+// const axios = require('axios');
+import axios from "axios";
 
 export default class blog extends Component{
+
+    state={
+        posts:[],
+    }
+
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) =>  {
+                this.setState({posts:response.data});
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+
     render(){
+
+        const postes = this.state.posts;
+        const allposts = postes.map((post,idx) =>{
+
+            return (
+                <div>
+                    <a href="">{idx +1}-{post.title}</a><br></br>
+                    <p>{post.body}</p>
+                </div>
+            )
+
+        });
+        
         return(
             <div>
-                <h1>Ini adalah blog punya component</h1>
+                <h1><marquee class="marq" bgcolor="Green" behavior="alternate" direction="left" loop="">Ini adalah blog punya component</marquee></h1>
+                <div class="d-flex text-center">
+                    <div class="flex-grow-1 ms-3">
+                    {allposts}
+                    </div>
+                </div>
             </div>
         )
     }
