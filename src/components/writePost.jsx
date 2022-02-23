7a16c8de0c80b3370be128b172df46af392df9fa
Amierul Izzuddin <amierulizzuddin21@gmail.com>
2022-02-23 22:49:25 +0800

    ActiveLink -css-

--- src/components/writePost.jsx
+++ src/components/writePost.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class profile extends Component{
+    
+    state={
+            title:'',
+            body:''
+    }
+
+    formSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/blog', { //where this url will be redirect
+            title: this.state.title, //all that value which its will take
+            body: this.state.body
+          })
+          .then(function (response) { //use for success
+            console.log(response);
+          })
+          .catch(function (error) { //use for error
+            console.log(error);
+          });
+    }
+    
     render(){
         return(
-            <div>
-                <h1>Ini adalah Write Post punya component</h1>
+            <center>
+            <div >
+            <div class="jumbotron col-lg-4 bg-primary" >
+
+            <form onSubmit={this.formSubmit}>
+            <div class="mb-3">
+                <label class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" onChange={event => {this.setState({title:event.target.value})}}/>
             </div>
+            <div class="mb-3">
+                <label class="form-label">Details</label>
+                <textarea class="form-control" name="body" onChange={event => {this.setState({body:event.target.value})}}>Write here </textarea>
+            </div>
+            
+            <button type="submit" class="btn btn-danger">Publish</button>
+            </form>
+            </div>
+            </div>
+            </center>
         )
     }
 
--- src/header.jsx
+++ src/header.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,NavLink} from "react-router-dom";
 import About from './components/about';
 import Blog from './components/blog';
 import Contact from './components/contact';
@@ -18,14 +18,14 @@ export default class header extends Component{
                 <div>
                     <Navbar bg="light" expand="lg">
                         <Container>
-                            <Navbar.Brand><Link to="/">Meh le belajau</Link></Navbar.Brand>
+                            <Navbar.Brand><NavLink to="/">Meh le belajau</NavLink></Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link ><Link to="/about">About kami</Link></Nav.Link>
-                                <Nav.Link ><Link to="/contact">Contact</Link></Nav.Link>
-                                <Nav.Link ><Link to="/blog">Blog</Link></Nav.Link>
-                                <Nav.Link ><Link to="/profile">Profile</Link></Nav.Link>
+                                <Nav.Link ><NavLink to="/about">About kami</NavLink></Nav.Link>
+                                <Nav.Link ><NavLink to="/contact">Contact</NavLink></Nav.Link>
+                                <Nav.Link ><NavLink to="/blog">Blog</NavLink></Nav.Link>
+                                <Nav.Link ><NavLink to="/profile">Profile</NavLink></Nav.Link>
                             </Nav>
                             </Navbar.Collapse>
                         </Container>
